8bd434af3be62230c6b0bc8e86e81da09843ce5f
ZarinaMye <113510101+ZarinaMye@users.noreply.github.com>
2022-11-21 14:40:12 +0100

    read me

---
+++ README.md
@@ -0,0 +1,6 @@
+Log in page
+
+Saved 
+users and passwords:
+janne     test
+zarina    lösen1
---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+
+//  <input type="text" id="userName">
+//<input type="text" id="passWord">
+//<button id="logInBtn">Log in</button>
