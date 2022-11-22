42bf62ffd4076f7407d552f8a520ed7234e07c54
ZarinaMye <113510101+ZarinaMye@users.noreply.github.com>
2022-11-22 16:17:14 +0100

    log in + fel/meddelande

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 
 
 
-//  <input type="text" id="userName">
-//<input type="text" id="passWord">
-//<button id="logInBtn">Log in</button>
+//  
+
+const findUser = document.getElementById("findUser");
+const logInBtn = document.getElementById("logInBtn");
+const logOutBtn = document.getElementById("logOutBtn");
+const printUserMessage = document.getElementById("userMessage");
+
+let users = [
+    {userName: "janne", passWord: "test"},
+    {userName: "zarina", passWord: "lösen1"},
+]  
+// object arrow
+
+logInBtn.addEventListener("click", () => {
+
+    let user = users.find(user => user.userName === printUserMessage.value);
+    console.log("user", user);
+ //varför hittas inte if...?
+    if (user) {
+        printUserMessage.innerHTML = "Welcome" + " " + user.userName +  "!";
+    } else
+    {
+        printUserMessage.innerHTML = "Ivalid user- and or password!";
+    }
+});
+   
+//användt find för att söka i object arrow:en
