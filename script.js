b23353589e096cdf054f36246c2127c4c2473a81
ZarinaMye <113510101+ZarinaMye@users.noreply.github.com>
2022-11-23 13:23:27 +0100

    text o psedu

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const findUserName = document.getElementById("UserName");
-//const findPassWord = document.getElementById("PassWord");
+const findPassWord = document.getElementById("PassWord");
 const logInBtn = document.getElementById("logInBtn");
 //const logOutBtn = document.getElementById("logOutBtn");
 //const newUserBtn = document.getElementById("newUserBtn");
@@ -18,15 +18,35 @@ document.body.appendChild(para);
 
 logInBtn.addEventListener("click", () => {
 
-    let user = users.find(user => user.userName === findUserName.value);
+    let user = users.find(user => user.userName === findUserName.value);//&& user.passWord === findPassWord.value);
     console.log("user", user);
  
     if (user) {
         printUserMessage.innerHTML = "Welcome" + " " + user.userName + " "+ "to your member page!";
-    } else
+        
+
+        //const logOutBtn = document.createElement("button")
+        //logOutBtn.innerHTML = <button>Log out</button>
+        //document.body.appendChild(menu)
+
+    } else if (user)
     {
         printUserMessage.innerHTML = "Ivalid user- and or password!";
-    }
+    } 
+
 });
 // find för att leta i object arrow
    
+/* //logInBtn.addEventListener("click", () => {
+    
+    if ( inputfält tomt){
+        "Hello! <br> If you are a member you know what to do, if not...
+    }
+    else if (user login true ) {
+        "Welcome" + " " + user.userName + " "+ "to your...
+        log out knapp finns
+        log in knapp och formulär borta
+    }
+    else (user log in false) {
+        "Ivalid user- and or password!";
+    }  */
