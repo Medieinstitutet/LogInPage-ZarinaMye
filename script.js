52dd76530ad2d373235cff45d9bb3e2ec016ed1c
ZarinaMye <113510101+ZarinaMye@users.noreply.github.com>
2022-11-23 18:11:40 +0100

    js from remember me

--- script.js
+++ script.js
@@ -45,20 +45,65 @@ logInBtn.addEventListener("click", () => {
     }
 });
 // find för att leta i object arrow
-   
-/* //logInBtn.addEventListener("click", () => {
 
-    if ( inputfält tomt){
-        "Hello! <br> If you are a member you know what to do, if not...
-        log in knapp o input formulär finns
-    }
-    else if (user login true ) {
-        "Welcome" + " " + user.userName + " "+ "to your...
-        log out knapp finns
-        log in knapp och formulär borta
-    }
-    else (user log in false) {
-        "Ivalid user- and or password!";
-        log in knapp o input formulär finns
-    }  */
+///////////////////////////////////////////////////////////////////////
 
+
+
+    //1.
+let saveNameBtn = document.getElementById("saveNameBtn")
+let inputName = document.getElementById("inputName")
+let showName = document.getElementById("showName")
+
+//5.Kolla om det finns ett sparat inlogg, 
+//om true kalla på printname om false kalla på printUnknown
+if (localStorage.getItem("userName")) {
+    printMemberPage();
+} else {
+    printHomePage();
+}
+
+saveNameBtn.addEventListener("click", () =>  {
+    //2.fånga inskrivet namn o spara i ls =(ny användare )
+    //(måste också gå att kolla om nått finns sparat i ls, typ mina användare)
+    let userName = inputName.value;
+    localStorage.setItem("userName", userName);
+    printMemberPage();
+
+});
+
+function printMemberPage () { 
+    //(render fumktion dvs skriver över gammalt) 
+    //3.hämta namn från ls o skriv ut (=välkomst sidan om inlogg finns)
+    let userName = localStorage.getItem("userName");
+    showName.innerText = "Welcome" + " " + userName;
+
+    //6.skapa och skriv ut en glömknapp(log out)
+    let logOutButton = document.createElement("button");
+    logOutButton.innerText = "Log out"
+    logOutButton.addEventListener("click", () => {
+        localStorage.removeItem("userName");
+        printHomePage();
+    });
+    showName.appendChild(logOutButton);
+
+
+    //(ingen! inlogg o inputfält!)
+
+}
+
+function printHomePage () {
+    //4.skriv ut att det inte finns ett namn sparat (sida med inlogg 2x inputfält och start)
+    showName.innerText = "You are unknown";  //=startsidan
+    // (inloggbtn+inputfält x2+ sign inbtn)
+    /* let logInButton = document.createElement("button");
+    logInButton.innerText = "Log in"
+    logInButton.addEventListener("click", () => {
+        localStorage.removeItem("userName");
+        printName();
+    });
+    showName.appendChild(logInButton); */
+
+}
+
+//function om felaktigt lösen () print....
